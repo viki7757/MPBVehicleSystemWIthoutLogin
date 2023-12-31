40f81e548ec3cf38a4b80f5d9c18502616c06fdf
viki7757 <vishallviki@proton.me>
2023-12-31 20:04:09 +0300

    Add MudBlazor / Created a VehicleRegistration page / Set rendering configuration / Register Controller & map controller in Program.cs

---
+++ MPBVehicleSystemWIthoutLogin/Controllers/VehicleRegistrationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MPBVehicleSystemWIthoutLogin.Controllers
+{
+    [Route("api/VehicleRegistration")]
+    [ApiController]
+    public class VehicleRegistrationController : ControllerBase
+    {
+    }
+}
