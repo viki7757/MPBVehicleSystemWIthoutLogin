9bf3edfdbabbbdc068cf89ea388b9fb8989e6622
viki7757 <vishallviki@proton.me>
2024-01-07 19:49:58 +0300

    Vehicle configuration page created(frontend) / stable

--- ClassLibrary1/Entities/VehicleLocation.cs
+++ ClassLibrary1/Entities/VehicleLocation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace MPBVehicleSystemWIthoutLogin.Shared.Entities
     public class VehicleLocation
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
     }
 }
---
+++ MPBVehicleSystemWIthoutLogin/Controllers/VehicleConfigurationFeatureController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MPBVehicleSystemWIthoutLogin.Data;
+
+namespace MPBVehicleSystemWIthoutLogin.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class VehicleConfigurationFeatureController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public VehicleConfigurationFeatureController( DataContext dataContext)
+        {
+            _dataContext = dataContext; 
+        }
+    }
+}
